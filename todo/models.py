53155e75ab91174cadaf70389824048aab1db095
abolfazl-sarijloo <abolsarijloo2007@gmail.com>
2025-06-06 22:25:40 +0330

    Initial commit 🚀

---
+++ todo/models.py
@@ -0,0 +1,27 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+# Create your models here.
+class Todo(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    title = models.CharField(max_length=200)
+    description = models.TextField()
+    completed = models.BooleanField(default=False)
+    date_created = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.user.username + " - " + self.title
+
+class TodoItem(models.Model):
+    todo = models.ForeignKey(Todo, on_delete=models.CASCADE)
+    title = models.CharField(max_length=200)
+    description = models.TextField()
+    completed = models.BooleanField(default=False)
+    date_created = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.todo.user.username + " - " + self.todo.title + '-' + self.title
+    
+    
+
+
